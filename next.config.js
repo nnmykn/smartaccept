db571773d080b41290b39d0dfe8814737c87e489
Kan Ninomiya <hi@kan.run>
2023-06-29 14:48:16 +0900

    除外

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Important: return the modified config
+    config.module.rules.push({
+      test: /\.tsx$/,
+      include: /past-system/,
+      use: 'null-loader'
+    })
+
+    return config
+  },
 }
 
 module.exports = nextConfig
